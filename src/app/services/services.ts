dbf2c1913c5e6f6180f5ecd1bb5a039ffe586960
Pavlos <pavloskoz@hotmail.com>
2025-09-11 07:32:28 +0200

    add some screenshots

--- src/app/services/services.ts
+++ src/app/services/services.ts
@@ -21,21 +21,21 @@ export class ServicesComponent {
     {
       title: 'Auth0',
       description: 'Have the best token based authentication in the industry with Auth0 for scalability and ease of use - offering us developer friendly SDks and more.',
-      screenshot: 'assets/screenshots/pqc-service.png',
+      screenshot: 'screenshots/auth0.png',
       technologies: ['Auth0', 'Typescript', 'Javascript'],
       price: ''
     },
     {
       title: 'Angular Web Applications',
       description: 'Modern, reactive and responsive web applications built with Angular, Material/Boostrap using industry best practices.',
-      screenshot: 'assets/screenshots/angular-service.png',
+      screenshot: 'screenshots/angular.png',
       technologies: ['Angular', 'TypeScript', 'Material Design', 'Bootstrap'],
       price: ''
     },
     {
       title: 'Python',
-      description: 'High-performance REST APIs with Python FastAPI, async support, and comprehensive documentation.',
-      screenshot: 'assets/screenshots/fastapi-service.png',
+      description: 'High-performance REST APIs with Python FastAPI, async support, and fast prototyping.',
+      screenshot: 'screenshots/python.png',
       technologies: ['Python', 'FastAPI'],
       price: ''
     },
